023b918f239fa13f8a929fe8026aa9243b2d84b2
רום סניטובסקי <romrom306@gmail.com>
2020-10-23 22:34:34 +0300

    first on

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.status(200).send('hello from server');
+}
+)
+
 
 const port=3000;
 app.listen(port , ()=>{
