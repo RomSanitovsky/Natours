7cb8b539efdd04b247043b3a1c83d1248530d745
רום סניטובסקי <romrom306@gmail.com>
2020-10-25 14:08:22 +0200

    update

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.status(200).send('hello from server');
+    res.status(200).json({Messege : 'wow' , mode : 2});
 }
-)
+);
+
+app.post('/',(req,res)=>{
+    res.send('this is post');
+}
+);
 
 
 const port=3000;
