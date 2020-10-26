15ad820afbb7f4c3fde1a7842ccce6c1b2d06457
Rom Sanitovsky <romrom306@gmail.com>
2020-10-26 18:30:59 +0200

    26/10 changes

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
+const fs = require('fs');
 const express = require('express');
+const { allowedNodeEnvironmentFlags } = require('process');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).json({Messege : 'wow' , mode : 2});
-}
-);
+app.use(express.json());
 
-app.post('/',(req,res)=>{
-    res.send('this is post');
-}
-);
+// app.get('/',(req,res)=>{
+//     res.status(200).json({Messege : 'wow' , mode : 2});
+// }
+// );
 
+// app.post('/',(req,res)=>{
+//     res.send('this is post');
+// }
+// );
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+});
+
+app.get('/api/v1/tours/:id',(req,res)=>{
+
+    console.log(req.params);
+    const id = req.params.id*1;
+    const tour = tours.find(el=> el.id === id);
+    if (!tour)
+    {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {  
+            tour
+        }
+     })
+});
+
+app.post('/api/v1/tours',(req,res)=>{
+    //console.log(req.body);
+    const newId = tours[tours.length-1].id +1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=> {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
 
 const port=3000;
 app.listen(port , ()=>{
