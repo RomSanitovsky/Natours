68297fab4143e703f927853ce42b3402092c3aea
Rom Sanitovsky <romrom306@gmail.com>
2020-10-29 16:45:00 +0200

    commit 29/10 2

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const userRouter = require('./routs/userRoutes');
 
 const app = express();
 
+console.log(process.env.NODE_ENV);
 // 1) Middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV==='development')
+{
+    app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 app.use((req,res,next)=> {
     console.log('hello from the middleware');
     next();
