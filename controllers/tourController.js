6bb7c97db7975f067fdb5f648a258768003c303f
Rom Sanitovsky <romrom306@gmail.com>
2020-11-05 01:32:56 +0200

    5/11

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -61,22 +61,50 @@ exports.createTour = async (req,res)=>{
         res.status(400).json({
             status: 'failed',
             message: err
-        })
+        });
     }
 };
 
-exports.updateTour = (req,res)=>{
-    res.status(200).json({
-        status: "success",
-        data: {
-            tour: "<update tour here..."
-        }
-    });
+exports.updateTour = async (req,res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id , req.body , {
+            new : true,
+            runValidators : true
+            
+        });
+        res.status(200).json({
+            status: "success",
+            data: {
+                tour
+            }
+        });
+    }
+
+    catch (err)
+    {
+        res.status(404).json({
+            status: 'failed',
+            message: err
+        });
+    }
+
+   
 };
 
-exports.deleteTour = (req,res)=>{
+exports.deleteTour = async (req,res)=>{
+    try{  
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
         status: "success",
-        data: null
     });
+}
+catch (err)
+{
+    res.status(404).json({
+        status: 'failed',
+        message: err
+    });
+}
+    
+
 };
