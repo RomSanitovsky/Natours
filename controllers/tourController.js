2f612a0cc5a6bfefea87f6c9acbd3f8a9420b832
Rom Sanitovsky <romrom306@gmail.com>
2020-11-08 21:42:56 +0200

    8/11

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const { models } = require('mongoose');
+const { models, Query } = require('mongoose');
 
 const Tour = require('../models/tourModel');
 
@@ -8,13 +8,42 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req,res)=>{
     try{
+    //BUILD QUERY
+
+    // 1A) FILTERING
     const queryObj = {...req.query};
     const excludedFields = ['page', 'sort' , 'limit' , 'fields'];
     excludedFields.forEach(element => {
         delete queryObj[element];
     });     
-    const tours = await Tour.find(queryObj);
+    // 1B) ADVANCED FILTERING
+    let queryStr= JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g , match => `$${match}` );
+    console.log(JSON.parse(queryStr));
 
+    let query = Tour.find(JSON.parse(queryStr));
+    // 2) Sorting
+    if (req.query.sort){
+        
+        const sortBy = req.query.sort.replace(',',' ');
+        query = query.sort(sortBy);
+    } else {
+        query.sort('-createdAt');
+    }
+    
+    // 3) Field Limiting
+    if (req.query.fields){
+        
+        const fields = req.query.fields.replace(',',' ');
+        query = query.select(fields);
+    } else {
+        query.select('-__v');
+    }
+
+
+    //EXECUTE QUERY
+    const tours = await query;
+ 
     res.status(200).json({
         status: 'success',
         results: tours.length,
