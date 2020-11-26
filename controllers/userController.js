9b4b2f7c86bd21208da92506cf9374fe44de5cee
Rom Sanitovsky <romrom306@gmail.com>
2020-11-26 22:15:52 +0200

    26/11

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,21 @@
 
+const User = require('../models/userModel');
+const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 
-
-exports.getAllUsers = (req,res)=>{
-    res.status(500).json({
-        status: 'error',
-        message: 'this route is not yet defined'
+exports.getAllUsers =  catchAsync(async (req,res)=>{
+    const users = await User.find();
+ 
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data:{
+            users
+        }
+        
     });
-};
+});
 exports.createUser = (req,res)=>{
     res.status(500).json({
         status: 'error',
