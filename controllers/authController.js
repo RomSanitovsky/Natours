b442de004b1e4d9686d2f79130066bb8c37a77a3
Rom Sanitovsky <romrom306@gmail.com>
2020-11-29 15:55:33 +0200

    29/11

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
-const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
 
 const signToken = id => {
