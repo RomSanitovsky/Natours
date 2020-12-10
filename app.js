00095a5681e0a715c7d72a1bb33efce769877b55
Rom Sanitovsky <romrom306@gmail.com>
2020-12-10 17:01:48 +0200

    10/12

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const GlobalErrorHandler = require('./controllers/errorController')
@@ -34,6 +37,21 @@ app.use('/api', limiter);
 /// Body parser, reading data from body into req.body
 app.use(express.json({limit: '10kb'}));
 
+// Data sanitization against NoSql 
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+// Prevent paramater pollution
+app.use(hpp({
+    whitelist: [
+        'duration', 
+        'ratingQuantity', 
+        'ratingAverage', 
+        'difficulty', 
+        'maxGroupSize', 
+        'price'
+    ]
+}));
 // Serving Static Files
 app.use(express.static(`${__dirname}/public`))
 
