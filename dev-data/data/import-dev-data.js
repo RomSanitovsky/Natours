bbd272c50872abe6e4e04118356c7232f4b9ec86
Rom Sanitovsky <romrom306@gmail.com>
2020-12-28 21:15:09 +0200

    28/12

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({path: './config.env'})
 
@@ -14,9 +16,14 @@ mongoose.connect(DB ,    {
     }).then(()=>{console.log("DB connection successful!")});
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json` , 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json` , 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json` , 'utf-8'));
+
 const importData = async ()=> {
     try{
         await Tour.create(tours);
+        await User.create(users, {validateBeforeSave: false});
+        await Review.create(reviews);
         console.log('data loaded successfully');
         process.exit();
     } catch (err) {
@@ -28,6 +35,8 @@ const importData = async ()=> {
 const deleteData = async ()=> {
     try{
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('data deleted successfully');
         process.exit();
     } catch (err) {
--- routs/reviewRoutes.js
+++ routs/reviewRoutes.js
@@ -4,10 +4,12 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
 .route('/')
 .get(reviewController.getAllReviews)
-.post(authController.protect,
+.post(
      authController.restrictTo('user'),
      reviewController.setTourUserIds,
      reviewController.createReview
@@ -16,8 +18,8 @@ router
 router
 .route('/:id')
 .get(reviewController.getReview)
-.delete(reviewController.deleteReview)
-.patch(reviewController.updateReview);
+.delete(authController.restrictTo('user','admin'), reviewController.deleteReview)
+.patch(authController.restrictTo('user','admin'), reviewController.updateReview);
 
 
 
